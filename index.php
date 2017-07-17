b10abd5ed035125b4c6d0dc69e252744bda300d4
madnoyz <madnoyz@gmail.com>
2017-07-17 12:04:54 -0700

    uploading files

---
+++ index.php
@@ -0,0 +1,403 @@
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Clyde Pabro</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <link href="css/style.css" rel="stylesheet" type="text/css" >
+
+</head>
+
+<body class="body" onload="draw();">
+
+
+    <nav class="navbar navbar-inverse navbar-fixed-bottom">
+        
+        <div class="container container-fluid">
+            
+            <div class="navbar-header">
+            
+                <a class="navbar-brand" href="#home"><strong style="font-size: 2em;">CLYDE PABRO</strong> &nbsp &nbsp <em>Developer . Designer . Rider</em></a>
+            
+            </div>
+        
+            <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+        
+                <ul class="nav navbar-nav active">
+                    <li><a href="#about">.about.</a></li>
+                    <li><a href="#work">.works.</a></li>
+                    <li><a href="#resume">.resume.</a></li>
+                    <li><a href="#hire">.hire me.</a></li>
+                </ul>
+        
+            </div>    
+
+        </div>
+    </nav>    
+
+
+    <div class="site-wrapper" id="home">
+
+        <div class="site-wrapper-inner">
+
+            <div class="container">
+
+                <div class="inner cover">
+
+                    <h1 class="cover-heading">aka Madnoyz.</h1><br />
+                    <h2 class="cover-heading"><span style="color: purple;">Full Stack Development</span><br />
+                    <span style="color: silver;">& Graphic Design</span></h2>
+                    <hr />
+                    <blockquote class="white-text">
+                    <p class="lead p-head"> I <u style="text-decoration-style: dotted;">design</u> & <mark style="background-color: #b0b0e0;">code</mark> web sites.<br /> 
+                    Also, I help <mark style="background-color: #b0b0e0;">find</mark> your <u style="text-decoration-style: dotted;">brand</u>, <u style="text-decoration-style: dotted;">users</u>, and <u style="text-decoration-style: dotted;">story.</u><br />
+                    Did I <u style="text-decoration-style: dotted;">mention</u> I know <mark style="background-color: #b0b0e0;">graphic</mark> <mark style="background-color: #b0b0e0;">design</mark> too?...</p>
+                    </blockquote>
+                    <br /><br /><br />
+                    <p class="lead">
+                        <h4> see how <br />
+                        <a href="#about">&nbsp &nbsp v</a></h4>
+                    </p>
+
+          
+                </div><!-- inner cover -->
+
+            </div><!-- container -->
+
+        </div><!-- site-wrapper-inner -->
+
+    </div><!-- site-wrapper -->
+
+
+    <div class="site-wrapper-about" id="about">
+
+        <div class="site-wrapper-inner">
+
+            <div class="container">
+
+                <div class="inner cover">
+
+                    <h2 class="cover-heading">What I do. What is a front-end developer?</h2><br />
+                    <p class="lead">I design the user experience, solve any problem, and create the application you want. From web design in photoshop to code in my MacOS, you dream it, i'll build it.</p>
+                    <br />
+
+                    <h2 class="cover-heading">Unsure of the website that you dreamed of?</h2><br />
+                    <p class="lead">Most do not know what they want, but it is my job to find your story, your problem, and fulfill your requirements. We will work together to find your website.</p>
+                    <br />
+
+                    <h2 class="cover-heading">Also, I specialize in Mobile Development.</h2><br />
+                    <p class="lead">I have designed a few mobile applications for my senior project and internships. I am gaining experience everyday with HTML, CSS, and JS frameworks to develop responsive mobile first projects. </p>
+                    <br />
+
+                    <h3 class="cover-heading">What to expect</h3>
+                    <div class="col-xs-6 col-md-4">
+                        <p><h4>Constant Communication</h4>
+                        You can ensure constant communication throughout the entire project. So we can be on the same page.</p>
+                    </div>
+
+                    <div class="col-xs-6 col-md-4">
+                        <p><h4>Quality Work</h4>
+                        As a professional graphic designer and university educated coder, you can expect quality work every time.</p>
+                    </div>
+
+                    <div class="col-xs-6 col-md-4">
+                        <p><h4>Still Undecided?</h4>
+                        I am more than happy to answer any question you may have regarding my design work?</p>
+                    </div>
+                    
+                    <br /><br />
+                </div><!-- inner cover -->
+
+            </div><!-- container -->
+
+        </div><!-- site-wrapper-inner -->
+
+    </div><!-- site-wrapper-about -->
+
+
+    <div class="site-wrapper-work" id="work">
+
+        <div class="site-wrapper-inner">
+
+            <div class="container">
+
+                <div class="inner cover">
+
+                    <h1 class="cover-heading">Works</h1><br />
+                    <br /><br />
+
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <li data-target="#myCarousel" data-slide-to="3"></li>
+                            <li data-target="#myCarousel" data-slide-to="4"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+
+                        <div class="item active">
+                            <a href="http://bbc.madnoyz.com" target=_blank>
+                            <img class="img-responsive center-block img-fluid" src="/img/bbc-screen.png" alt="BBC" width="460" height="345">
+                            <br /><br /><br /><br /><br /><br />
+                            <div class="carousel-caption">
+                                <h3>BBC</h3>
+                                <p>I tried to mock design the BBC website.</p></a>
+                            </div>
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="http://reaction.madnoyz.com">
+                            <img class="img-responsive center-block img-fluid" src="img/reaction-screen.png" alt="Reaction" width="460" height="345">
+                            <br /><br /><br /><br /><br /><br />
+                            <div class="carousel-caption">
+                                <h3>Reaction</h3>
+                                <p>Coding a Javascript Game.</p>
+                            </div>
+                            </a>
+                        </div>
+    
+                        <div class="item">
+                            <a href="http://mysql.madnoyz.com" target=_blank>
+                            <img class="img-responsive center-block img-fluid" src="img/php-mysql-logo.png" alt="PHP/MySQL" width="460" height="345">
+                            <br /><br /><br /><br /><br /><br />
+                            <div class="carousel-caption">
+                                <h3>PHP/MySQL</h3>
+                                <p>Learning about RESTful API and PHP/MySQL.</p>
+                            </div>
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="img/web_interfaces_cst_by_madnoyz.png" target=_blank>
+                            <img class="img-responsive center-block img-fluid" src="img/web_interfaces_cst_by_madnoyz.png" alt="Flower" width="460" height="345">
+                            <br /><br /><br /><br /><br /><br />
+                            <div class="carousel-caption">
+                                <h3>Website Design</h3>
+                                <p>A Photoshop mockup for a website side design.</p>
+                            </div>
+                            </a>
+                        </div>
+
+                        <div class="item">
+                            <a href="https://drive.google.com/file/d/0Bzl6LxpTGQBMRC1pVV9qQnc1dkk/view?usp=sharing" target=_blank>
+                            <img class="img-responsive center-block img-fluid" src="img/cali.png" alt="Flower" width="460" height="345">
+                            <br /><br /><br /><br /><br /><br />
+                            <div class="carousel-caption">
+                                <h3>Motion Design</h3>
+                                <p>Animated motion video of Locations in California for a client.</p>
+                            </div>
+                            </a>
+                        </div>
+  
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+
+                        </div>
+                    </div>
+
+                   
+                </div><!-- inner cover -->
+
+            </div><!-- container -->
+
+        </div><!-- site-wrapper-inner -->
+
+    </div><!-- site-wrapper-work -->
+
+
+    <div class="site-wrapper-resume" id="resume">
+
+        <div class="site-wrapper-inner">
+
+            <div class="container">
+
+                <div class="inner cover resume-cover">
+
+                    <h1 class="cover-heading"><a href="assets/Clyde_Pabro_Full_Stack_Resume.pdf" target=_blank>Resume</a></h1>
+                    <h3 class="cover-heading"><span style="color: purple;">Summary</span></h3>
+                    <p class="lead">A student in Full Stack Web Development with experience and extensive education in software design and web development. A fast learner with strong time management and multitasking skills. Effective work ethic in a team or individual setting to drive product success and process efficiency. Strong troubleshooting and problem solving skills with an analytical and curious mindset. </p>
+               
+                    <h3 class="cover-heading"><span style="color: purple;">Education</span></h3>
+
+                    <p class="lead"><span class="cover-heading"><strong>Sacramento State University</strong>, Sacramento, CA,             
+                    <em>Bachelor’s of Science Computer Science</em><br /> SEP 2014 - MAY 2017</p></span>
+
+                    <p class="lead"><span class="cover-heading"><strong>Folsom Lake College</strong>, Folsom, CA              
+                    <em>Associate’s Degree in Computer Science and Mathematics</em><br /> MAY 2008 - MAY 2014</p></span>
+
+                    <h3 class="cover-heading"><span style="color: purple;">Work Experience</span></h3>
+                    <p class="lead"><span class="cover-heading"><strong>City of West Sacramento</strong>, West Sacramento, CA              
+                    <br /><em>APPLICATION DEVELOPER</em>, &nbsp SEP 2016 - JUNE 2017</p></span>
+                    <p class="lead">Developed a mobile application for Mayor Christopher L. Cabaldon for use within the community. SCRUM software development methodology and modern web technologies were utilized in the creation of this app.</p>
+
+                    <p class="lead"><em>GRAPHIC PRODUCTION ARTIST</em>, &nbsp OCT 2013 - SEP 2016</p></span>
+                    <p class="lead">Managed the development of creative solutions through visual communication. Lead the graphic design and implementation of corporate advertising campaigns and concepts for print/web media. Also, photographed various media events. </p>
+
+                    <p class="lead"><span class="cover-heading"><strong>Murphy's Magic Supplies, Inc. (<a href="http://murphysmagic.com" target=_blank>murphysmagic.com</a>)</strong>, Rancho Cordova, CA              
+                    <br /><em>GRAPHIC PRODUCTION ARTIST</em>, &nbsp NOV 2008 - MAY 2011</p></span>
+                    <p class="lead">Designed and developed graphics for website, user interfaces, and advertising. Implemented a dvd-to-web conversion protocol for use with their online streaming business.</p>
+
+
+                </div><!-- inner cover -->
+
+            </div><!-- container -->
+
+        </div><!-- site-wrapper-inner -->
+
+    </div><!-- site-wrapper-resume -->
+
+
+        <div class="site-wrapper-hire" id="hire">
+
+        <div class="site-wrapper-inner">
+
+            <div class="container">
+
+                <div class="inner cover">
+
+                    <h1 class="cover-heading"><u>Hire Me!</u></h1><br />
+                    <h3 class="cover-heading">tl;dr</h3>
+                    <p class="lead">Filipino, web developer, graphic designer,<br />creative, coder, gamer, harley davidson <br />motorcycle enthusiasts, ice cream lover, <br />versatile, new techie, over-thinker, perfectionist.</p>
+                
+                    <div class="row">	
+
+                        <div class="emailForm">	
+                            <h1>Contact Me</h1>
+                            <?php echo $result; ?>  
+
+                            <form method="post">	
+
+                                <div class="form-group">	
+
+                                    <label for="name">Your Name:</label>	
+                                    <input type="text" name="name" class="form-control" placeholder="Your Name" value="<?php echo $_POST['name']; ?>" />    
+
+                                </div>	
+
+                                <div class="form-group">	
+
+                                    <label for="email">Your Email:</label>	
+                                    <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $_POST['email']; ?>" />   
+
+                                </div>	
+
+                                <div class="form-group">	
+
+                                    <label for="comment">Your Comment:</label>	
+                                    <textarea class="form-control" name="comment"><?php echo $_POST['comment']; ?></textarea>	
+                            
+                                </div>	
+
+                                <input type="submit" name="submit" class="btn btn-default btn-lg" value="Submit" />	
+
+
+                            </form><!-- end of form -->	
+
+                        </div><!-- emailForm -->	
+
+                    </div><!-- row -->	
+
+                </div><!-- inner cover -->
+
+            </div><!-- container -->
+
+        </div><!-- site-wrapper-inner -->
+
+    </div><!-- site-wrapper -->
+
+
+    <script>
+
+        $.ajax({
+            url:"works.html"
+        }).done(function(data) {
+            alert(data);
+        });
+
+</script>
+
+<?php
+    
+if ($_POST["submit"]) {
+    if (!$_POST['name']) {
+        $error="<br />Please enter your name";
+    }
+            
+    if (!$_POST['email']) {
+        $error.="<br />Please enter your email address";
+    }
+            
+    if (!$_POST['comment']) {
+    }
+            
+    $error.="<br />Please enter a comment";
+    
+    if ($_POST['email']!="" and !filter_var($_POST['email'],
+    FILTER_VALIDATE_EMAIL)) {
+         $error.="<br />Please enter a valid email address";
+    }
+            
+    if ($error) {
+        $result='<div class="alert alert-danger"><strong>There were error(s)
+in your form:</strong>'.$error.'</div>';
+    } else {
+        if (mail("baliztikarts@yahoo.com", "Comment from website!", "Name: ".
+        $_POST['name']."	
+	
+Email: ".$_POST['email']."	
+	
+Comment: ".$_POST['comment'])) {
+            $result='<div class="alert alert-success"><strong>Thank
+you!</strong> I\'ll be in touch.</div>';
+        } else {
+            $result='<div class="alert alert-danger">Sorry, there was
+an error sending your message. Please try again later.</div>';
+        }
+    }
+}
+    
+?>
+
+    <!-- Javascript for fixed navbar for the frontpage -->
+    <script src="js/navbar-fixed.js" type="text/javascript"></script>
+    
+    <!-- Canvas script for animation of the frontpage -->
+    <script src="js/canvas.js" type="text/javascript"></script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript" ></script>  
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+
+
